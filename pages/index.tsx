538dd8e1ed8eb93b29c0c46830057de9098f0da9
Matthew Francis <mattfrancis888@gmail.com>
2021-11-16 21:26:46 -0500

    Updated index.tsx.

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,8 @@ const Home: NextPage = () => {
 
             <main className={styles.main}>
                 <h1 className={styles.title}>
-                    Welcome to <a href="https://nextjs.org">Next.js!</a>
+                    Welcome to{" "}
+                    <a href="https://nextjs.org">Next.js! It is cool!</a>
                 </h1>
 
                 <p className={styles.description}>
